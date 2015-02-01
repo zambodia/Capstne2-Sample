bfb674d52e578b5e0cf5de2ea24782bc32bc824a
zambodia <zambodia222@gmail.com>
2015-02-01 17:05:52 +0800

    Test

--- Mattsample/Assets/Scripts/TestScript3.cs
+++ Mattsample/Assets/Scripts/TestScript3.cs
@@ -5,11 +5,8 @@ public class TestScript3 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
 	//YOYOYO
-=======
-	
->>>>>>> parent of 427eaa7... TestScript3 - I was here
+
 	}
 	
 	// Update is called once per frame

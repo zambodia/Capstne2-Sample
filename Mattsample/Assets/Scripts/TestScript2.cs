50ee7964c01e6063df5d34c281c58cd97da99dae
zambodia <zambodia222@gmail.com>
2015-02-01 17:13:14 +0800

    lol

--- Mattsample/Assets/Scripts/TestScript2.cs
+++ Mattsample/Assets/Scripts/TestScript2.cs
@@ -1,15 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class TestScript2 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> parent of bfb674d... Test
 	}
 	
 	// Update is called once per frame
--- Mattsample/Assets/Scripts/TestScript3.cs
+++ Mattsample/Assets/Scripts/TestScript3.cs
@@ -5,16 +5,8 @@ public class TestScript3 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
 	//I'm now here
 
-=======
-<<<<<<< HEAD
-	//YOYOYO
-=======
-	
->>>>>>> parent of 427eaa7... TestScript3 - I was here
->>>>>>> parent of bfb674d... Test
 	}
 	
 	// Update is called once per frame

276ab26d33009f016b7d715df5b8eb1853a4977e
zambodia <zambodia222@gmail.com>
2015-02-01 17:34:02 +0800

    Revert "delete files"
    This reverts commit dcfe9caef69d92166b0537608048d38988b1c097.

---
+++ Mattsample/Assets/Scripts/Class/_EnemyAIStats.cs
@@ -0,0 +1,94 @@
+﻿//********************Project Luxon********************//
+//Created by:       Matthew Fabic                      //
+//Date created:     January 22, 2014                   //
+//Edited by:                                           //
+//Date edited:                                         //
+//*****************************************************//
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Luxon {
+
+    public class _EnemyAIStats : MonoBehaviour {
+
+        //for enemy's stat
+        private int maxHP;
+        private int currentHP;
+        private float Attack;
+        private float Defense;
+        private float Speed;
+        private float dodgeRate;
+        private float critRate;
+        private int expReward;
+
+        //constructor
+        public _EnemyAIStats() {
+            this.maxHP = 420;
+            this.currentHP = 420;
+            this.Attack = 48;
+            this.Defense = 29;
+            this.Speed = 35;
+            this.dodgeRate = 15;
+            this.critRate = 10;
+            this.expReward = 106;
+        }
+        
+        //setting the stats of enemy based on its' type
+        public void SelectEnemyType(int _chosenType) {
+            if (_chosenType == 1) {         //Weak Enemy
+                this.maxHP = 420;
+                this.currentHP = 420;
+                this.Attack = 48;
+                this.Defense = 29;
+                this.Speed = 35;
+                this.dodgeRate = 15;
+                this.critRate = 10;
+                this.expReward = 106;
+            }
+            else if (_chosenType == 2) {    //Average Enemy
+                this.maxHP = 512;
+                this.currentHP = 512;
+                this.Attack = 57;
+                this.Defense = 45;
+                this.Speed = 32;
+                this.dodgeRate = 15;
+                this.critRate = 10;
+                this.expReward = 117;
+            }
+            else if (_chosenType == 3) {    //Tough Enemy
+                this.maxHP = 620;
+                this.currentHP = 620;
+                this.Attack = 69;
+                this.Defense = 51;
+                this.Speed = 30;
+                this.dodgeRate = 15;
+                this.critRate = 10;
+                this.expReward = 137;
+            }
+        }
+
+        #region Basic Getter
+        public int GetMaxHP()  { return this.maxHP; }
+        public int GetCurrentHP() { return this.currentHP; }
+        public float GetAttack() { return this.Attack; }
+        public float GetDefense() { return this.Defense; }
+        public float GetSpeed() { return this.Speed; }
+        public float GetCritRate() { return this.critRate; }
+        public float GetDodgeRate() { return this.dodgeRate; }
+        public int GetExpReward() { return this.expReward; }
+        #endregion
+
+        //applying damage taken by the player to enemy HP
+        public int ApplyDamage(float _playerDamage) {
+            int damage;
+            //compute for the damage taken
+            damage = (int) ((_playerDamage * 2) - this.Defense);
+            //subtract damage to current HP
+            return this.currentHP -= damage;
+        }
+
+    }
+
+}
---
+++ Mattsample/Assets/Scripts/WeakEnemySampleScript.cs
@@ -0,0 +1,114 @@
+﻿//********************Project Luxon********************//
+//Created by:       Matthew Fabic                      //
+//Date created:     January 22, 2014                   //
+//Edited by:                                           //
+//Date edited:                                         //
+//*****************************************************//
+
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class WeakEnemySampleScript : MonoBehaviour {
+
+    Luxon._EnemyAIStats myEnemy = new Luxon._EnemyAIStats();
+
+    public GameObject CombatManager;
+    private CombatManagerScript CombatScript;
+
+    public int EnemyType; //0 = weak, 1 = average, 2 = tough enemy
+
+    private Vector3 startingPos;
+    public Transform PlayerPos;
+
+    private Vector3 vel;
+
+    private int maxHP;
+    private int currentHP;
+    private float Attack;
+    private float Defense;
+    private float Speed;
+    private int expReward;
+
+    private bool startAttackAnim;
+    public bool addDamage;
+    public bool enemyAttack;
+
+	// Use this for initialization
+	void Start () {
+        CombatScript = CombatManager.GetComponent<CombatManagerScript>();
+        int temp = EnemyType + 1;
+        this.myEnemy.SelectEnemyType(temp);
+        startingPos = this.transform.position;
+
+        startAttackAnim = false;
+        addDamage = false;
+        enemyAttack = false;
+        vel = Vector3.zero;
+	}
+
+    void OnMouseDown() {
+        CombatScript.SelectedEnemyID = this.EnemyType;
+        Debug.Log("EnemyType: " + this.EnemyType);
+    }
+
+    //add damage to enemy
+    public void AddDamage(int _damage) {
+        this.myEnemy.ApplyDamage(_damage);
+    }
+
+    #region Getter
+    public int GetCurrentHP() { return currentHP; }
+    public int GetMaxHP() { return maxHP; }
+    public int GetAttack() { return (int)Attack; }
+    public int GetDefense() { return (int)Defense; }
+    public int GetSpeed() { return (int)Speed; }
+    public int GetExpReward() { return (int)expReward; }
+    #endregion
+
+    //sets the local variables based on the values on _EnemyAIStats class
+    void CheckStats() {
+        this.maxHP = this.myEnemy.GetMaxHP();
+        this.currentHP = this.myEnemy.GetCurrentHP();
+        this.Attack = this.myEnemy.GetAttack();
+        this.Defense = this.myEnemy.GetDefense();
+        this.Speed = this.myEnemy.GetSpeed();
+        this.expReward = this.myEnemy.GetExpReward();
+
+        if (this.currentHP <= 0) {
+            this.currentHP = 0;
+            Debug.Log("Enemy has died.");
+            Destroy(this.gameObject);
+        }
+    }
+
+    //simple enemy attack animation
+    IEnumerator StartAttackSequence() {
+        this.transform.position = Vector3.SmoothDamp(this.transform.position, PlayerPos.transform.position, ref vel, 0.75f);
+        yield return new WaitForSeconds(0.25f);
+        this.transform.position = Vector3.SmoothDamp(this.transform.position, startingPos, ref vel, 0.75f);
+        yield return new WaitForSeconds(0.25f);
+        startAttackAnim = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        CheckStats();
+
+        //if (Input.GetKeyUp(KeyCode.L)) {
+        //    startAttackAnim = true;
+        //    addDamage = true;
+        //}
+
+        if (addDamage)
+            startAttackAnim = true;
+
+        if (startAttackAnim == true) {
+            StartCoroutine(StartAttackSequence());
+        }
+
+        if (Input.GetKeyUp(KeyCode.W)) {
+            this.myEnemy.ApplyDamage(68);
+        }
+	}
+}

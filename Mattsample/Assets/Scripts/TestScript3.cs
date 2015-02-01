6d2e66b3a72a4605efdb982262d2b0b391318be1
zambodia <zambodia222@gmail.com>
2015-02-01 17:01:31 +0800

    Sample

---
+++ Mattsample/Assets/Scripts/TestScript3.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class TestScript3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+<<<<<<< HEAD
+	//YOYOYO
+=======
+	
+>>>>>>> parent of 427eaa7... TestScript3 - I was here
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
